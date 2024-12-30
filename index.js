8635a3872e2342b2e09e6364d1589cb6ad90907c
richardMattsson <mattsson.richard@gmail.com>
2024-12-30 14:06:53 +0100

    changing the project list to images of the projects.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("hej");
 
 const title = document.querySelector("#title");
 const ul = document.querySelector("#ul");
+const main = document.querySelector("#main");
 
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
@@ -15,22 +16,27 @@ if (title.textContent === "Projects") {
   const shoppingList = {
     name: "Project on making a simple shoppinglist",
     href: "https://richardmattsson.github.io/shoppingList/",
+    img: "shoppingList.png",
   };
   const ericaPaintings = {
     name: "Example website for a painter artist",
     href: "https://richardmattsson.github.io/Erica/",
+    img: "erica.png",
   };
   const soccerStats = {
     name: "My site about european fotball",
     href: "https://richardmattsson.github.io/SoccerStats/",
+    img: "soccerStat.png",
   };
   const blackJack = {
     name: "The card game 'Black-jack'",
     href: "https://richardmattsson.github.io/blackjack2/",
+    img: "blackjack.png",
   };
   const weatherApp = {
     name: "WeatherApp",
     href: "https://richardmattsson.github.io/weatherApp/",
+    img: "weather.png",
   };
 
   createProjectLinks(weatherApp);
@@ -40,12 +46,18 @@ if (title.textContent === "Projects") {
   createProjectLinks(shoppingList);
 
   function createProjectLinks(project) {
-    const li = document.createElement("li");
+    const img = document.createElement("img");
+    img.setAttribute("src", project.img);
+    img.classList.add("projectImg");
+    // const li = document.createElement("li");
     const a = document.createElement("a");
     a.classList.add("projectLinks");
-    li.textContent = project.name;
+    // li.textContent = project.name;
+    // li.appendChild(img);
     a.setAttribute("href", project.href);
-    a.appendChild(li);
-    ul.appendChild(a);
+    // a.appendChild(li);
+    a.appendChild(img);
+    // ul.appendChild(a);
+    main.appendChild(a);
   }
 }
